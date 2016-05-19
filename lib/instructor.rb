027a848793b6d5c5c34a0acfd2af186fafdc3f6d
Gerard (Gerry) Caulfield <git@caulfield.me>
2016-05-19 23:12:56 +1000

    Add Instructor message chaining
    As with the addition of method chaining to the Robot class,
    I am only doing this directly as this class isn't yet being
    used by anybody other than myself.

--- lib/instructor.rb
+++ lib/instructor.rb
@@ -4,9 +4,6 @@ require './lib/robot'
 
 # Dispatches recieved commands to appropriate parts of the system.
 class Instructor
-  extend Forwardable
-  def_delegators :@robot, :left, :right
-
   def self.build(width: 5, height: 5)
     platform = Table.new(width, height)
     robot = Robot.build
@@ -21,14 +18,26 @@ class Instructor
   def place(x_pos, y_pos, direction)
     if @platform.valid_position?(x_pos, y_pos)
       @robot.place(x_pos, y_pos, direction)
+      self
     else
       raise OutsideBounds,
             "Error: coordinates are not on the table [#{x_pos},#{y_pos}]"
     end
   end
 
+  def left
+    @robot.left
+    self
+  end
+
+  def right
+    @robot.right
+    self
+  end
+
   def move
     @robot.move { |x_pos, y_pos| @platform.valid_position?(x_pos, y_pos) }
+    self
   rescue InvalidMove => exception
     puts exception.message
   end
